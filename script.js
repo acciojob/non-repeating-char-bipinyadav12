1dca4e0437904e93ee92fcfa7b3635a78f46bce2
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-07 11:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+const count={};
+	for (char of str)
+		{
+			if(count[char])
+			 {
+				count[char]+=1;
+			 }
+			   count[char]+1;
+		}
+	if (count[char]===1)
+	{
+		return char;
+	}
+	else {
+		return null;
+	}
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
