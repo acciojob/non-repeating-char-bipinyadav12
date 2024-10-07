1fa8cd0d03c6dc8bb6c5224bcab03cf3e3c2d846
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-07 11:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const count={};
 	for (let char of str)
 		{
 			if(count[char])
-			 {
-				count[char]+=1;
-			 }
-			else{
-			   count[char]=1;
-			}
-		}
-	for (let char of str){
-	if (count[char]===1)
-	{
-		return char;
-	}
-	}
+			   {
+				  count[char] +=1;
+			   }
+			else
+			  {
+			   count[char] = 1;
+			  }
+		 }
+	for (let char of str)
+	   {
+	     if (count[char]===1)
+	       {
+		     return char;
+	       } 
+	   }
 		return null;
-	
-	
 }
 //const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
